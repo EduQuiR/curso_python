a9b610d3655f4618fbbb0e7523db4375f6c6e285
Eduardo <eduandresq67@gmail.com>
2024-02-27 16:49:46 -0300

    Modificaciones en los ejercicios

---
+++ escribir_lineas.py
@@ -0,0 +1,3 @@
+import sys
+
+print('hola como estas \'pao\'')
---
+++ script.py
@@ -0,0 +1,9 @@
+import sys
+
+# Nos permite anilizar los datos que le pasamos a un script durante una ejecucion atraves de la linea de comandos
+print(sys.argv[1:])
+
+
+
+
+
